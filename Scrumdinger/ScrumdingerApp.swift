c679ddd3b1cf15539c92b6c1c95320fe55b047ea
Dustin Pfannenstiel <dmp@hey.com>
2020-12-18 08:06:26 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Dustin Pfannenstiel on 12/18/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
