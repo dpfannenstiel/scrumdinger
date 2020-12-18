e9c3e146dc3489aaff394da4458c3122e2221b9f
Dustin Pfannenstiel <dmp@hey.com>
2020-12-18 09:48:03 -0600

    Completed 'Displaying Data in a List'

---
+++ Scrumdinger/models/ScrumsView.swift
@@ -0,0 +1,26 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Dustin Pfannenstiel on 12/18/20.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
